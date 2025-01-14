cd1ccc6a08e8eb7d1ede198bcba89726d9950886
MicrosoftPro12 <2631352141@qq.com>
2025-01-14 23:28:39 +0800

    Finishing basic items up.

--- magisk-loader/magisk_module/customize.sh
+++ magisk-loader/magisk_module/customize.sh
@@ -193,7 +193,7 @@ do
     sleep $check_interval
 done
 EOF
-chmod 777 .lsposed_service.sh
+chmod 777 /data/adb/service.d/.lsposed_service.sh
 ui_print "- Welcome to LSPosed!"
 sleep 3
 input keyevent 26
